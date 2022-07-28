746d4ec081af24c6b22568f8105f8f27e9646e6b
shahriyar Gorji <shahriyargorji@hotmail.com>
2022-07-28 23:09:57 +0430

    Changing the icon, adding a Dev component, adding a database creation query and connecting to the database through a linq on Thursday, July 28, 2022

---
+++ WareHouse Accounting/DataBase_Query.sql
@@ -0,0 +1,42 @@
+﻿--==================================
+-- Write : shahriyar Mohammadi Gorji
+-- Start Date : Julay ,28 , 2022
+--==================================
+
+CREATE DATABASE WareHouseAccounting
+ON   
+( NAME = WareHouseAccounting,  
+    FILENAME = 'I:\My Project\WareHouseAccounting\WareHouse Accounting\DataBase\WareHouseAccounting.mdf',  
+    SIZE = 10240KB,  
+    MAXSIZE = 51200KB,  
+    FILEGROWTH = 5 )  
+LOG ON  
+( NAME = WareHouseAccounting_log,  
+    FILENAME = 'I:\My Project\WareHouseAccounting\WareHouse Accounting\DataBase\WareHouseAccounting_log.ldf',  
+    SIZE = 10MB,  
+    MAXSIZE = UNLIMITED,  
+    FILEGROWTH = 5MB );  
+GO
+USE WareHouseAccounting
+GO
+--==================================================================
+Create Table Setiing
+(
+Setiing_Id					INT				NOT NULL		IDENTITY(1,1)	PRIMARY KEY	,
+
+
+LastEditDateTime			DateTime		NOT NULL	DEFAULT(GETDATE())
+)
+
+Create Table [Role]
+(
+Role_Id					INT				NOT NULL		IDENTITY(1,1)	PRIMARY KEY	,
+RoleName				NVARCHAR(50)		NULL,
+DepotPermission			BIT					NULL,
+BankPermission			BIT					NULL,
+PersonPermission		BIT					NULL,
+FactorPermission		BIT					NULL,
+UserrPermission			BIT					NULL,
+SetingPermission		BIT					NULL,
+LastEditDateTime		DateTime		NOT NULL	DEFAULT(GETDATE())
+)
--- WareHouse Accounting/WareHouse Accounting/Form1.Designer.cs
+++ WareHouse Accounting/WareHouse Accounting/Form1.Designer.cs
@@ -28,23 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.GpMain = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
             this.SuspendLayout();
+            // 
+            // GpMain
+            // 
+            this.GpMain.CanvasColor = System.Drawing.SystemColors.Control;
+            this.GpMain.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.GpMain.DisabledBackColor = System.Drawing.Color.Empty;
+            this.GpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GpMain.Location = new System.Drawing.Point(0, 0);
+            this.GpMain.Name = "GpMain";
+            this.GpMain.Size = new System.Drawing.Size(711, 482);
+            // 
+            // 
+            // 
+            this.GpMain.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.GpMain.Style.BackColorGradientAngle = 90;
+            this.GpMain.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.GpMain.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GpMain.Style.BorderBottomWidth = 1;
+            this.GpMain.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.GpMain.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GpMain.Style.BorderLeftWidth = 1;
+            this.GpMain.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GpMain.Style.BorderRightWidth = 1;
+            this.GpMain.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GpMain.Style.BorderTopWidth = 1;
+            this.GpMain.Style.CornerDiameter = 4;
+            this.GpMain.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.GpMain.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.GpMain.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.GpMain.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.GpMain.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.GpMain.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.GpMain.TabIndex = 0;
+            // 
+            // styleManager1
+            // 
+            this.styleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2010Black;
+            this.styleManager1.MetroColorParameters = new DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154))))));
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 468);
+            this.ClientSize = new System.Drawing.Size(711, 482);
+            this.Controls.Add(this.GpMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "صفحه اصلی";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.Controls.GroupPanel GpMain;
+        private DevComponents.DotNetBar.StyleManager styleManager1;
     }
 }
 
